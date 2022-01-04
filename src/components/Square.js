8ba3bdd73d0da2f5e8c45dfa08d5b8921d3dc342
Chava888 <salvador_tena@hotmail.com>
2022-01-04 15:48:31 -0800

    created a 3 by 3 grid board game with a question mark in each square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 
-class Square extends Component{
-  render(){
-    return(
+class Square extends Component {
+  render() {
+    return (
       <>
+        <div className="square">{this.props.value}</div>
       </>
     )
   }
